e4258180d65a635df07cd377c67b313c99bc0ff6
lipon <liponbdbd@gmail.com>
2025-05-21 11:43:58 +0600

    installed gsap animation library and setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import GsapPractice from "@/components/GsapPractice";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div className=" h-screen w-full flex justify-center items-center bg-slate-50 text-slate-700">
-    <span >Hi web animation</span>
+    {/* <span >Hi web animation</span> */}
+    <GsapPractice/>
    </div>
   );
 }
---
+++ components/GsapPractice.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useEffect, useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+const GsapPractice = () => {
+  return (
+    <div>
+      Hi GSAP
+    </div>
+  )
+}
+
+export default GsapPractice
